6903d2eeb0383d8e19e5127e8f1f02ae71323251
Lydia Zhang <lydiaxzhang@Lydias-MacBook-Pro.local>
2021-03-28 14:53:25 -0400

    Mapping multiple points

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -4,16 +4,6 @@ console.log("working");
 // Create the map object with a center and zoom level
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
-// Add a marker to the map for Los Angeles, California
-// let marker = L.marker([34.0522, -118.2437]).addTo(map);
-
-// Change the marker to a point or dot
-// L.circleMarker([34.0522, -118.2437], {
-//     radius: 300,
-//     color: "black",
-//     fillColor: "#ffffa1"
-//  }).addTo(map);
-
 // Get data from cities.js
 let cityData = cities;
 
